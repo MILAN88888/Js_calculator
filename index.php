4f73d5bf190154893659440f219749316a48a022
Milan Chaudhary <109399375+MILAN88888@users.noreply.github.com>
2023-01-08 14:53:19 +0545

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<html>
-<frameset cols=" 50%,50%" border=0px>
-<frame src="calculator.php" noresize></frame>
-<frameset rows="60%, 40%" >
-<frame src="quote.php" noresize></frame>
-<frame src="alarm.php" noresize></frame>
-</frameset>
-</frameset>
-</html>
